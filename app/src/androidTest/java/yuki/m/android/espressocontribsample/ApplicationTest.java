8c5c2f4607bebb4991e2487002a6ed1cadbb22b5
MatsumuraYuki <define1394@gmail.com>
2015-05-21 21:59:21 +0900

    write code and test

--- app/src/androidTest/java/yuki/m/android/espressocontribsample/ApplicationTest.java
+++ app/src/androidTest/java/yuki/m/android/espressocontribsample/ApplicationTest.java
@@ -16,14 +16,26 @@
 
 package yuki.m.android.espressocontribsample;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void RecyclerViewItemのクリック() {
+        onView(withId(R.id.list)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, click()));
     }
 }
--- app/src/main/java/yuki/m/android/espressocontribsample/MainActivity.java
+++ app/src/main/java/yuki/m/android/espressocontribsample/MainActivity.java
@@ -18,13 +18,72 @@ package yuki.m.android.espressocontribsample;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new RecyclerViewAdapter(recyclerView));
+    }
+
+    private static class RecyclerViewAdapter
+            extends RecyclerView.Adapter<RecyclerViewAdapter.MainViewHolder> {
+        private List<String> datasource
+                = Arrays.asList("data1", "data2", "data3", "data4", "data5", "data6", "data7", "data8", "data9");
+        private RecyclerView recyclerView;
+
+        static class MainViewHolder extends RecyclerView.ViewHolder {
+            private TextView titleTextView;
+
+            public MainViewHolder(View itemView) {
+                super(itemView);
+                titleTextView = (TextView)itemView.findViewById(android.R.id.text1);
+            }
+        }
+
+        public RecyclerViewAdapter(RecyclerView recyclerView) {
+            this.recyclerView = recyclerView;
+        }
+
+        @Override
+        public RecyclerViewAdapter.MainViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+            v.setOnClickListener(itemClickListener);
+            return new MainViewHolder(v);
+        }
+
+        private final View.OnClickListener itemClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "click pos " + recyclerView.getChildPosition(v), Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        @Override
+        public void onBindViewHolder(RecyclerViewAdapter.MainViewHolder view, int position) {
+            view.titleTextView.setText(datasource.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return datasource.size();
+        }
     }
 }
